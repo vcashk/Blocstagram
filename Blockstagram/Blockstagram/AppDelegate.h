eecb70aa9f5444207e1b48d01b4a94a09472d578
vcashk <vcashk@gmail.com>
2015-11-01 10:31:00 +0400

    added the delete functionality for the row

---
+++ Blockstagram/Blockstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blockstagram
+//
+//  Created by Vikash Khanna on 10/31/15.
+//  Copyright © 2015 Vikash Khanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blockstagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  Blockstagram
+//
+//  Created by Vikash Khanna on 10/31/15.
+//  Copyright © 2015 Vikash Khanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
